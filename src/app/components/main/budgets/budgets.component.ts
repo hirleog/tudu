51bcec9de6d90e083e475899fa4dbca2fcd52266
guilherme hirle <guilherme.hirle1@gmail.com>
2025-02-09 11:30:45 -0300

    budgets layout done

--- src/app/components/main/budgets/budgets.component.ts
+++ src/app/components/main/budgets/budgets.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Budget } from 'src/app/interfaces/budgets';
 
 @Component({
   selector: 'app-budgets',
@@ -6,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budgets.component.css']
 })
 export class BudgetsComponent implements OnInit {
+  budgets: Budget[] = [
+    {
+      id: 1,
+      photo: '../../../../assets/aline.PNG', // Ícone FontAwesome
+      name: 'Aline',
+      rate: '4',
+      serviceComplete: '66',
+      price: '39.45',
+      distance: '1.5',
+      distanceMinutes: '60'
+    },
+    {
+      id: 2,
+      photo: '../../../../assets/matheus.PNG', // Ícone FontAwesome
+      name: 'Matheus',
+      rate: '5',
+      serviceComplete: '15',
+      price: '45.54',
+      distance: '1.0',
+      distanceMinutes: '8'
+    },
+    {
+      id: 3,
+      photo: '../../../../assets/GUI.PNG',
+      name: 'Guilherme',
+      rate: '3',
+      serviceComplete: '234',
+      price: '33.00',
+      distance: '4.6',
+      distanceMinutes: '24'
+    }
+  ];
 
   constructor() { }
 
---
+++ src/app/interfaces/budgets.ts
@@ -0,0 +1,10 @@
+export interface Budget {
+  id: number;
+  photo: string;
+  name: string;
+  rate: string;
+  serviceComplete: string;
+  price: string;
+  distance: string;
+  distanceMinutes: string;
+}
