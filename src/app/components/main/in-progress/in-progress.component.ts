6dac190fb94c719e16bc480ff122357d339641a4
guilherme hirle <guilherme.hirle1@gmail.com>
2025-02-14 23:08:09 -0300

    progress flow init

--- src/app/components/main/in-progress/in-progress.component.ts
+++ src/app/components/main/in-progress/in-progress.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InProgressCard } from 'src/app/interfaces/progress-card';
 
 @Component({
   selector: 'app-in-progress',
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./in-progress.component.css']
 })
 export class InProgressComponent implements OnInit {
+
+  cards: InProgressCard[] = [
+    {
+      name: 'Aline',
+      photo: '../../../../assets/aline.PNG', // Ícone FontAwesome
+      dateTime: '' // Adicionamos esta propriedade
+    },
+    {
+      name: 'Matheus',
+      photo: '../../../../assets/matheus.PNG', // Ícone FontAwesome
+      dateTime: '' // Adicionamos esta propriedade
+    },
+    {
+      name: 'Guilherme',
+      photo: '../../../../assets/GUI.PNG',
+      dateTime: '' // Adicionamos esta propriedade
+    }
+  ];
 
   constructor() { }
 
---
+++ src/app/interfaces/progress-card.ts
@@ -0,0 +1,5 @@
+export interface InProgressCard {
+    name: string;
+    photo: string;
+    dateTime: string;
+}
