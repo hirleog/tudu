4e907ee5e2017dc312508e87ac806a785d36a23e
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-04-18 18:03:30 -0300

    apiurl added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   mfeUrl: 'https://tudu-professional.web.app', // URL do MFE em produção
-  apiUrl: 'https://89.116.73.70:3000',
+  apiUrl: 'https://api.use-tudu.com.br',
 };
