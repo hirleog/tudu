799d16f3c4013c0f69e364d4918f7c6f20e2c9e5
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-04-19 00:24:14 -0300

    adição da url de produção do tudu-professional

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  mfeUrl: 'https://professional.use-tudu.com.br', 
+  mfeUrl: 'https://tudu-professional.web.app', // URL do MFE em produção
   apiUrl: 'https://api.use-tudu.com.br',
 };
