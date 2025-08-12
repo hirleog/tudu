a80a259412dc47f0e98818722d1e70542c73069f
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-08-12 16:48:24 -0300

    teste para api de pagamento

--- src/app/components/payments/payments.component.ts
+++ src/app/components/payments/payments.component.ts
@@ -171,25 +171,25 @@ export class PaymentsComponent {
     this.backToOffer.emit(indicator);
   }
 
-  //   pagar() {
-  //   const payload = {
-  //     card_number: '5155901222280001',
-  //     customer_id: 'customer_123',
-  //     amount: 1000,
-  //     order_id: 'order_123',
-  //     cardholder_name: 'JOAO DA SILVA',
-  //     expiration_month: '12',
-  //     expiration_year: '25',
-  //     security_code: '123',
-  //     brand: 'Mastercard',
-  //     first_name: 'Joao',
-  //     last_name: 'Silva',
-  //     email: 'joao@example.com',
-  //   };
+    pagar() {
+    const payload = {
+      card_number: '5155901222280001',
+      customer_id: 'customer_123',
+      amount: 1000,
+      order_id: 'order_123',
+      cardholder_name: 'JOAO DA SILVA',
+      expiration_month: '12',
+      expiration_year: '25',
+      security_code: '123',
+      brand: 'Mastercard',
+      first_name: 'Joao',
+      last_name: 'Silva',
+      email: 'joao@example.com',
+    };
 
-  //   this.paymentService.pagarComCartao(payload).subscribe({
-  //     next: (res) => console.log('Pagamento autorizado:', res),
-  //     error: (err) => console.error('Erro no pagamento:', err),
-  //   });
-  // }
+    this.paymentService.pagarComCartao(payload).subscribe({
+      next: (res) => console.log('Pagamento autorizado:', res),
+      error: (err) => console.error('Erro no pagamento:', err),
+    });
+  }
 }
