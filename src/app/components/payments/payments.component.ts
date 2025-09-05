8204cbf7ca83365897a11bfa41db590e16376b0e
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-09-05 17:50:44 -0300

    ajuste

--- src/app/components/payments/payments.component.ts
+++ src/app/components/payments/payments.component.ts
@@ -237,7 +237,7 @@ export class PaymentsComponent implements OnInit {
           transaction_type:
             this.selectedInstallmentOption.installments === 1
               ? 'FULL'
-              : 'install_With_Interest',
+              : 'install_with_interest',
           number_installments: this.selectedInstallmentOption.installments || 1,
           amount_installment: this.selectedInstallmentOption.installmentValue,
           soft_descriptor: 'TUDU Serviços',
