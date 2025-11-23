871d4c474d35cc34da81c8384fb832b282b83579
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-11-23 16:44:44 -0300

    ajustes

--- src/app/components/main/app-home/app-home.component.ts
+++ src/app/components/main/app-home/app-home.component.ts
@@ -147,6 +147,7 @@ export class AppHomeComponent implements OnInit {
 
     try {
       console.log('Solicitando subscription...');
+      console.log('Solicitando subscription...');
 
       this.swPush
         .requestSubscription({
