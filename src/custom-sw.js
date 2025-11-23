f72e5a1d44469a35bd627b8a04287437b9d8809f
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-11-23 10:41:20 -0300

    novas configs para PWA

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -1,15 +1,23 @@
+// REPLICA O SEU CÓDIGO AQUI
 self.addEventListener("push", (event) => {
+  console.log("[SW] Push recebido:", event);
+
+  if (!event.data) {
+    console.log("[SW] Push sem payload");
+    return;
+  }
+
   const data = event.data.json();
 
-  event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: data.icon,
-      data: {
-        url: data.url,
-      },
-    })
-  );
+  const options = {
+    body: data.body,
+    icon: data.icon || "assets/icons/icon-192x192.png",
+    badge: "assets/icons/badge-72x72.png",
+    vibrate: [200, 100, 200],
+    data: { url: data.url },
+  };
+
+  event.waitUntil(self.registration.showNotification(data.title, options));
 });
 
 self.addEventListener("notificationclick", (event) => {
