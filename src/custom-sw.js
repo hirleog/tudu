d69ceddb4abdc02de4c3d3c95d95b77338e8e5e6
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-11-23 11:15:55 -0300

    ajustes pwa

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -1,4 +1,6 @@
-// REPLICA O SEU CÓDIGO AQUI
+// ==========================
+//   RECEBIMENTO DO PUSH
+// ==========================
 self.addEventListener("push", (event) => {
   console.log("[SW] Push recebido:", event);
 
@@ -12,15 +14,53 @@ self.addEventListener("push", (event) => {
   const options = {
     body: data.body,
     icon: data.icon || "assets/icons/icon-192x192.png",
-    badge: "assets/icons/badge-72x72.png",
-    vibrate: [200, 100, 200],
-    data: { url: data.url },
+    badge: data.badge || "assets/icons/badge-72x72.png",
+    vibrate: data.vibrate || [200, 100, 200],
+    requireInteraction: data.requireInteraction ?? true, // Mantém a notificação presa até interação
+    data: {
+      url: data.url,
+    },
+
+    // ANDROID HEADS-UP PUSH 🔥🔥🔥
+    // Deixa como push prioridade máxima, igual Instagram
+    tag: "tudu-push",
+    renotify: true,
+    actions: [
+      {
+        action: "open",
+        title: "Abrir",
+      },
+    ],
   };
+
+  // Alguns devices Android exigem explicitamente o channelId
+  if (data.channelId) {
+    options.channelId = data.channelId;
+  }
 
   event.waitUntil(self.registration.showNotification(data.title, options));
 });
 
+// ==========================
+//   CLICK NA NOTIFICAÇÃO
+// ==========================
 self.addEventListener("notificationclick", (event) => {
+  console.log("[SW] Notificação clicada:", event.notification);
   event.notification.close();
-  event.waitUntil(clients.openWindow(event.notification.data.url));
+
+  // Abre ou foca aba já aberta
+  event.waitUntil(
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientsList) => {
+        for (const client of clientsList) {
+          if (client.url === event.notification.data.url && "focus" in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(event.notification.data.url);
+        }
+      })
+  );
 });
